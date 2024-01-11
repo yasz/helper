da13e9fdc0a8a131586f4b704df0cc1723388f51
yasz <yaszyoyo@gmail.com>
2024-01-11 12:55:33 +0800

    feat

---
+++ src/main/java/tool/CodingTest.java
@@ -0,0 +1,8 @@
+package tool;
+
+public class CodingTest {
+    public static void main(String[] args) {
+        System.out.println("0信1息2信息3");
+        System.out.println(System.getProperty("file.encoding"));
+    }
+}
